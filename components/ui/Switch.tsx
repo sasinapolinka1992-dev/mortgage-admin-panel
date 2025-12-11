e5d9cb9cc8d8aff377236fcc4e2d0524e5f005c4
sasinapolinka1992-dev <sasina.polinka.1992@gmail.com>
2025-12-11 14:44:39 +0500

    Update Switch.tsx

--- components/ui/Switch.tsx
+++ components/ui/Switch.tsx
@@ -9,14 +9,15 @@ interface SwitchProps {
 export const Switch: React.FC<SwitchProps> = ({ checked, onChange, label }) => {
   return (
     <div className="inline-flex items-center">
-        <div className="UNI_checkbox">
+        {/* Используем label, чтобы клик по "рисунку" переключателя активировал input */}
+        <label className="UNI_checkbox cursor-pointer">
             <input 
                 type="checkbox" 
                 checked={checked} 
                 onChange={(e) => onChange(e.target.checked)} 
             />
             <div className="fake"></div>
-        </div>
+        </label>
         {label && <span className="ml-3 text-sm font-medium text-gray-700 cursor-pointer" onClick={() => onChange(!checked)}>{label}</span>}
     </div>
   );
